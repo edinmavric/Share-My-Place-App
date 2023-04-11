2c1c542578d488f991c7b5ef0a6f4bac63f8029d
Edin Mavric <edinmavric10@gmail.com>
2023-04-11 14:34:59 +0200

    added feedback

--- dist/assets/scripts/App.js
+++ dist/assets/scripts/App.js
@@ -1,3 +1,35 @@
+class Modal {
+  constructor(contentId, fallbackText) {
+    this.fallbackText = fallbackText;
+    this.contentTemplateEl = document.getElementById(contentId);
+    this.modalTemplateEl = document.getElementById("modal-template");
+  }
+
+  show() {
+    if ("content" in document.createElement("template")) {
+      const modalElements = document.importNode(
+        this.modalTemplateEl.content,
+        true
+      );
+      const modalElement = modalElements.querySelector(".modal");
+      const backdropElement = modalElements.querySelector(".backdrop");
+      const contentElement = document.importNode(
+        this.contentTemplateEl.content,
+        true
+      );
+
+      modalElement.appendChild(contentElement);
+      document.body.insertAdjacentElement("afterbegin", modalElement);
+      document.body.insertAdjacentElement("afterbegin", backdropElement);
+    } else {
+      // fallback code
+      alert(this.fallbackText);
+    }
+  }
+
+  hide() {}
+}
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
